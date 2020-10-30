6917be244e57266a968e857c175dd48690289fe3
m.shepherd <55757863+shepherdm1@users.noreply.github.com>
2020-10-30 13:51:46 -0400

    set up methods ConvexHull.java

--- src/ConvexHull.java
+++ src/ConvexHull.java
@@ -1,6 +1,19 @@
+import java.awt.*;
+import java.util.ArrayList;
 
 public class ConvexHull {
+    public static ArrayList<Point> hullCalc(ArrayList<Point> p){
+        ArrayList<Point> l= new ArrayList<>();
+
+        /// logic here
+
+
+
+        return l;
+    }
+
     public static void main(String[] args){
-        System.out.println("hello");
+        ArrayList<Point> p= new ArrayList<>();
+        hullCalc(p);
     }
 }
