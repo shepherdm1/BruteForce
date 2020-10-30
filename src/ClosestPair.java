6a24f02578d13bd08090deff7dc0397a5e5849b3
taniasajjad <48694852+taniasajjad@users.noreply.github.com>
2020-10-30 14:17:34 -0400

    Update ClosestPair.java

--- src/ClosestPair.java
+++ src/ClosestPair.java
@@ -2,11 +2,7 @@
 public class ClosestPair {
     public static void main(String[] args){
         int d ;
-        int x1 = 2;
-        int x2 = -2;
-        int y1 = 2;
-        int y2 = -3;
-        
+        ArrayList<Point> arrL = new ArrayList<Point>();
         int formula = (int) (Math.sqrt(Math.pow((x1-x2),2))+ (Math.pow((y1-y2),2)));
         for(i=1;i < n; i++ ){
         
