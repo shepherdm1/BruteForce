22723b9d2144b074768dbec7940f85fdbba16dc4
taniasajjad <48694852+taniasajjad@users.noreply.github.com>
2020-10-30 14:38:07 -0400

    Update ClosestPair.java

--- src/ClosestPair.java
+++ src/ClosestPair.java
@@ -9,15 +9,15 @@ public class ClosetPair {
      */
     
     public static Integer closestPoint(ArrayList<Point> p){
-        ArrayList<Point> arrL = new ArrayList<Point>();
+    ArrayList<Point> arrL = new ArrayList<Point>();
 
-        Integer d = null ;
-        int formula = (int) ();
+        Double d = null ;
+        
 
 
         for(int i=1;i < p.size(); i++ ){
             for(int j=i+1;j < p.size();j++ ){
-                d = (Math.min(d, Math.sqrt(Math.pow((p.get(i).x-x2),2))+ (Math.pow((y1-y2),2))));
+                d = (Math.min(d, Math.sqrt(Math.pow((p.get(i).x-p.get(j).x),2)) + (Math.pow((p.get(i).y-p.get(j).y),2))));
                 
             }
         }
