6c05baeb59c0c671ba1b0cbeb3f51a76cae01b90
m.shepherd <55757863+shepherdm1@users.noreply.github.com>
2020-11-02 15:37:16 -0500

    moved Will's test.java code into ConvexHull.java after he got it working. Minor changes made to label console output

--- src/ConvexHull/ConvexHull.java
+++ src/ConvexHull/ConvexHull.java
@@ -3,100 +3,80 @@ package ConvexHull;
 import java.awt.*;
 import java.util.ArrayList;
 
-
 public class ConvexHull {
-    public static ArrayList<Point> hullCalc(ArrayList<Point> p){
-        ArrayList<Point> l= new ArrayList<>();
+    public static ArrayList<Point> convexHull(ArrayList<Point> points) {
+        ArrayList<Point> output = new ArrayList<>();
 
-        for(int i = 0; i<p.size() - 1; i++){
-            for(int j = i+1;j<p.size();j++){
-                Point p1 = p.get(i);
-                Point p2 = p.get(j);
+        for (int i = 0; i < points.size() - 1; i++) {
+            for (int j = i + 1; j < points.size(); j++) {
+                boolean problem = false;
                 int sign = 0;
 
+                Point p1 = points.get(i);
+                Point p2 = points.get(j);
+
                 int x1 = (int) p1.getX();
-                int y1 = (int) p1.getY();
                 int x2 = (int) p2.getX();
-                int y2 = (int) p2.getX();
+                int y1 = (int) p1.getY();
+                int y2 = (int) p2.getY();
 
-                int a = y2-y1;
-                int b = x1-x2;
-                int c = (x1*y2)-(y1*x2);
+                int a = y2 - y1;
+                int b = x1 - x2;
+                int c = (x1 * y2) - (y1 * x2);
 
-                boolean foundProblem = false;
+                for (int k = 0; k < points.size(); k++) {
+                    Point p3 = points.get(k);
 
-                int tmp = (int) Double.POSITIVE_INFINITY;
+                    int x3 = (int) p3.getX();
+                    int y3 = (int) p3.getY();
 
-                for (int k = 0; k < p.size(); k++) {
-                    Point p3 = p.get(k);
-                    int region = a* (int) p3.getX() + b* (int) p3.getY() - c;
+                    int region = (a * x3) + (b * y3) - c;
 
                     if (sign != 0) {
-                        if (region > 0 && sign < 0) {
-                            foundProblem = true;
-                            break;
-                        } else if (region < 0 && sign > 0) {
-                            foundProblem = true;
+
+                        if (region > 0 && sign < 0 ||  region < 0 && sign > 0) {
+                            problem = true;
                             break;
                         }
+
                     } else {
                         if (region > 0) {
                             sign = 1;
-                        } else if (region < 0) {
-                            sign = -1;
-                        }
-                    }
-
-                }
-
-                if (!foundProblem) {
-                    if (!l.contains(p1)) {
-                        l.add(p1);
-                    }
-                    if (!l.contains(p2)) {
-                        l.add(p2);
+                        } else sign = -1;
                     }
                 }
 
-                /*for(int k=0;k<p.size();k++){
-                    int x3 = (int) p.get(k).getX();
-                    int y3 = (int) p.get(k).getY();
-                    if(!(x3==x1&&y3==y1)&&!(x3==x2&&y3==y2)){
-                        int check = a*p.get(k).x + b*p.get(k).y-c;
-                        if(tmp==(int) Double.POSITIVE_INFINITY){
-                            tmp = check;
-                        }
-                        else if ((tmp>=0&&check<0)||(tmp<0&&check>=0)){
-                            foundProblem = true;
-                            break;
-                        }
+                if (!problem) {
+                    if (!output.contains(p1)) {
+                        output.add(p1);
+                    }
+                    if (!output.contains(p2)) {
+                        output.add(p2);
                     }
                 }
-                if(!foundProblem){
-                    l.add(new Point[]{p.get(i), p.get(j)});
-                }*/
             }
         }
-        return l;
+
+        return output;
     }
-    public static void main(String[] args){
-        ArrayList<Point> p= new ArrayList<>();
-        for(int i = 0; i<10; i++){
-            int randx = (int)(Math.random() * 10) + 1;
-            int randy = (int)(Math.random() * 10) + 1;
-            Point pnt = new Point(randx,randy);
+
+    public static void main(String[] args) {
+        ArrayList<Point> p = new ArrayList<>();
+        System.out.println("10 Randomly generated points:");
+        for (int i = 0; i < 10; i++) {
+            int randx = (int) (Math.random() * 10) + 1;
+            int randy = (int) (Math.random() * 10) + 1;
+            Point pnt = new Point(randx, randy);
             p.add(pnt);
             System.out.println(pnt.toString().replace("java.awt.Point", "").replace("[", "(").replace("]", ")").replace("x=", "").replace("y=", ""));
         }
 
-        ArrayList<Point> solution = hullCalc(p);
+        ArrayList<Point> solution = convexHull(p);
 
-        System.out.println("Output: ");
-        for (Point point :  solution) {
+        System.out.println("\nPoints that make up convex hull given generated points:");
+        for (Point point : solution) {
             System.out.printf("(%.1f, %.1f)", point.getX(), point.getY());
         }
 
-        /*for (int z = 0; z< ConvexHull.hullCalc(p).size(); z++){
-            System.out.println(hullCalc(p).get(z)[0].toString().replace("java.awt.Point", "").replace("[", "(").replace("]", ")").replace("x=", "").replace("y=", "") + " - " + hullCalc(p).get(z)[1].toString().replace("java.awt.Point", "").replace("[", "(").replace("]", ")").replace("x=", "").replace("y=", ""));        }*/
     }
 }
